f56f91b02e7a20578bbc642e2bb4e43e4b6fba33
mashannsome <131888090+mashannsome@users.noreply.github.com>
2026-02-07 22:26:54 +0700

    Enhance install.sh to include external tool setup
    Add installation of external tools and their requirements

--- install.sh
+++ install.sh
@@ -3,4 +3,9 @@ pkg update -y
 pkg install python git -y
 pip install -r requirements.txt
 chmod +x osintv5.sh
+
+echo "Installing external tools..."
+git clone https://github.com/kovinevmv/getcontact.git external/getcontact
+pip install -r external/getcontact/requirements.txt
+
 echo "[+] Install selesai"
